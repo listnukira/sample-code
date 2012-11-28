4c9517e0a896967bac054738166387af95442b8f
chwhsu <listnukira@gmail.com>
2012-11-28 18:38:34 +0800

    add commit

--- get_ip_v2.c
+++ get_ip_v2.c
@@ -10,17 +10,21 @@ int main() {
 	struct sockaddr_in sock_addr;
 	int i;
 
+	// 取得 hostname
 	gethostname(hostname, sizeof(hostname));
 	printf("hostname: %s\n", hostname);
 
+	// 根據 hostname 取得 hostent struct
 	host = gethostbyname(hostname);
 
+	// 印出 hostname 的 alias
 	printf("\nhost name alias list:\n");
 	for (i = 0; host->h_aliases[i]; ++i) {
 		printf("%s\n", host->h_aliases[i]);
 	}
 
 	// h_addr_list point to (struct in_addr)
+	// 印出所有的 ip address
 	printf("\nip address list:\n");
 	for (i = 0; host->h_addr_list[i]; ++i) {
 		sock_addr.sin_addr = *((struct in_addr*) host->h_addr_list[i]);
