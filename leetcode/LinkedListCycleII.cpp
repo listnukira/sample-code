5476f7c2c98dd328a84949afeb1f9b4c2db460c5
Chih-Wei Hsu <wei_hsu@fiberlogic.com>
2014-09-02 11:33:00 +0800

    add LinkedListCycleII.cpp

---
+++ leetcode/LinkedListCycleII.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x): val(x), next(NULL) {}
+};
+
+ListNode* detectCycle(ListNode *head)
+{
+    struct ListNode* fast = head;
+    struct ListNode* slow = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        fast = fast->next->next;
+        slow = slow->next;
+
+        if (fast == slow)
+            break;
+    }
+
+    if (fast == NULL || fast->next == NULL)
+        return NULL;
+
+    slow = head;
+    while (slow != fast) {
+        fast = fast->next;
+        slow = slow->next;
+    }
+
+    return fast;
+}
+
+int main(int argc, char const* argv[])
+{
+    struct ListNode* head;
+    struct ListNode* ans;
+    struct ListNode node1(1);
+    struct ListNode node2(2);
+    struct ListNode node3(3);
+    struct ListNode node4(4);
+
+    head = &node1;
+    node1.next = &node2;
+    node2.next = &node3;
+    node3.next = &node4;
+
+    ans = detectCycle(head);
+    if (ans != NULL)
+        cout << ans->val << endl;
+    else
+        cout << "No cycle" << endl;
+    
+    return 0;
+}
